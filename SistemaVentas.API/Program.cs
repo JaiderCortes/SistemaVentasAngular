1a8eeb442b988a6565b80bcf5c26103469539ee9
JaiderCortes <jacortessa@gmail.com>
2024-07-21 22:05:52 -0500

    Creación de dependencias e inyección de dependencias del proyecto para la manipulación de los datos.

--- SistemaVentas.API/Program.cs
+++ SistemaVentas.API/Program.cs
@@ -1,3 +1,4 @@
+using SistemaVentas.IOC;
 
 namespace SistemaVentas.API
 {
@@ -13,6 +14,8 @@ namespace SistemaVentas.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.InyectarDependencias(builder.Configuration);
 
             var app = builder.Build();
 
---
+++ SistemaVentas.DAL/Repositorios/VentasRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVentas.DAL.DBContext;
+using SistemaVentas.DAL.Repositorios.Contrato;
+using SistemaVentas.Model;
+
+namespace SistemaVentas.DAL.Repositorios
+{
+    public class VentasRepository : GenericRepository<Ventas>, IVentaRepository
+    {
+        private readonly DbVentasContext _dbContext;
+
+        public VentasRepository(DbVentasContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Ventas> Registrar(Ventas modelo)
+        {
+            Ventas venta = new Ventas();
+            using var transaction = _dbContext.Database.BeginTransaction();
+            try
+            {
+                //Para restar la cantidad en stock de cada producto de la venta.
+                foreach (DetalleVenta dv in modelo.DetalleVenta)
+                {
+                    Productos productoEncontrado = _dbContext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                    productoEncontrado.Stock -= dv.Cantidad;
+                    _dbContext.Productos.Update(productoEncontrado);
+
+                }
+                await _dbContext.SaveChangesAsync();
+
+                //Para guardar la numeración del último documento.
+                NumerosDocumento correlativo = _dbContext.NumerosDocumentos.First();
+                correlativo.UltimoNumero += 1;
+                correlativo.FechaRegistro = DateTime.Now;
+                _dbContext.NumerosDocumentos.Update(correlativo);
+                await _dbContext.SaveChangesAsync();
+
+                //Para crear la numeración
+                int cantDigitos = 4;
+                string ceros = string.Concat(Enumerable.Repeat("0", cantDigitos));
+                string numVenta = ceros + correlativo.UltimoNumero.ToString();
+                numVenta = numVenta.Substring(numVenta.Length - cantDigitos, cantDigitos);
+                modelo.NumeroDocumento = numVenta;
+                await _dbContext.Ventas.AddAsync(modelo);
+                await _dbContext.SaveChangesAsync();
+
+                venta = modelo;
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+            return venta;
+        }
+    }
+}
