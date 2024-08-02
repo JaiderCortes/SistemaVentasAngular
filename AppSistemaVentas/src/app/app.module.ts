0a4792ac6b3395826169ae35bad95b49fc36b6f9
JaiderCortes <jacortessa@gmail.com>
2024-08-01 21:19:36 -0500

    Ajustes en implementación de rutas en el proyecto.

--- AppSistemaVentas/src/app/app.module.ts
+++ AppSistemaVentas/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './Reutilizable/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [],
 })
