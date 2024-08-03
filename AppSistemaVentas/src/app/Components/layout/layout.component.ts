49dd9200500eb0974aba71aa22db0fca740d5561
JaiderCortes <jacortessa@gmail.com>
2024-08-03 15:37:47 -0500

    Avances.

--- AppSistemaVentas/src/app/Components/layout/layout.component.ts
+++ AppSistemaVentas/src/app/Components/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../Reutilizable/shared/shared.module';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterOutlet],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
